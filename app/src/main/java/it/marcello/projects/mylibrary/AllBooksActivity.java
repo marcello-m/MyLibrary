823e9328a47165be922b19c91340b534876b445f
marcello-m <marcello-m@users.noreply.github.com>
2022-05-22 14:48:28 +0200

    first commit, basic app layout

---
+++ app/src/main/java/it/marcello/projects/mylibrary/AllBooksActivity.java
@@ -0,0 +1,32 @@
+package it.marcello.projects.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class AllBooksActivity extends AppCompatActivity {
+
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+        booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1,"Ready Player One", "Ernest Cline", 460, "https://www.ibs.it/images/9788804741831_0_536_0_75.jpg",
+                "Stunning Sci-Fi Novel", "Long Description"));
+        adapter.setBooks(books);
+    }
+}
